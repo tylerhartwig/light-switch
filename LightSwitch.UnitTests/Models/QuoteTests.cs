1e1892ebe88472e8f13455049ad1ebe49a1242fb
Tyler Hartwig <Tyler_hartwig@baylor.edu>
2017-03-15 13:08:18 -0500

    Add Quote Model

---
+++ LightSwitch.UnitTests/Models/QuoteTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LightSwitch.UnitTests
+{
+	[TestFixture]
+	public class QuoteTests
+	{
+		[Test]
+		public void TestDefaultConstructor()
+		{
+			var quote = new Quote();
+			Assert.NotNull(quote);
+		}
+
+		[Test]
+		public void TestGetSetText()
+		{
+			var text = "test text";
+			var quote = new Quote();
+			quote.Text = text;
+			Assert.AreEqual(text, quote.Text);
+		}
+
+		[Test]
+		public void TestGetSetReference()
+		{
+			var reference = "test reference";
+			var quote = new Quote();
+			quote.Reference = reference;
+			Assert.AreEqual(reference, quote.Reference);
+		}
+	}
+}
---
+++ LightSwitch/Models/Quote.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace LightSwitch
+{
+	public class Quote
+	{
+		public string Text { get; set; }
+		public string Reference { get; set; }
+
+		public Quote()
+		{
+		}
+	}
+}
