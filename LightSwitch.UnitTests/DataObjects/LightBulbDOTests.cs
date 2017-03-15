5092eb06c7ce833c1c1cb6dca814e7a0a2c5b3c8
Tyler Hartwig <Tyler_hartwig@baylor.edu>
2017-03-15 13:09:08 -0500

    Refactor LightBulb database object by renaming

---
+++ LightSwitch.UnitTests/DataObjects/LightBulbDOTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LightSwitch.UnitTests
+{
+	[TestFixture]
+	public class LightBulbDOTests
+	{
+		[Test]
+		public void TestLightBulbDefaultConstructor()
+		{
+			var lightBulb = new LightBulbDO();
+			Assert.NotNull(lightBulb);
+		}
+
+		[Test]
+		public void TestEqualObjects()
+		{
+			var lightBulb1 = new LightBulbDO { ID = 1 };
+			var lightBulb2 = new LightBulbDO { ID = 1 };
+
+			Assert.AreEqual(lightBulb1, lightBulb2);
+		}
+
+		[Test]
+		public void TestUnEqualObjects()
+		{
+			var lightBulb1 = new LightBulbDO { ID = 0 };
+			var lightBulb2 = new LightBulbDO { ID = 1 };
+
+			Assert.AreNotEqual(lightBulb1, lightBulb2);
+		}	}
+}
---
+++ LightSwitch/DataObjects/LightBulbDO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using SQLite;
+
+namespace LightSwitch
+{
+	public class LightBulbDO
+	{
+		public LightBulbDO() { }
+
+		[PrimaryKey, AutoIncrement]
+		public int ID { get; set; }
+
+		public override bool Equals(object obj)
+		{
+			var leftBulb = this;
+			var rightBulb = (LightBulbDO)obj;
+
+			return leftBulb.ID.Equals(rightBulb.ID);
+		}
+
+		public override int GetHashCode()
+		{
+			return ID;
+		}
+	}
+}
