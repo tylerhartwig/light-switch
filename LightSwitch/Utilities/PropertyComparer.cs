6600ca859aeb21378aaf12edd02c990c04a18cd1
Tyler Hartwig <Tyler_hartwig@baylor.edu>
2017-03-17 22:09:18 -0500

    Add utility for checking equality of properties between two objects

---
+++ LightSwitch/Utilities/PropertyComparer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace LightSwitch
+{
+	public class PropertyComparer<T> : IEqualityComparer<T>
+	{
+		public bool Equals(T x, T y)
+		{
+			Type type = x.GetType();
+			var typeInfo = type.GetTypeInfo();
+
+			var properties = typeInfo.DeclaredProperties;
+			foreach (var property in properties)
+			{
+				var xValue = property.GetValue(x);
+				var yValue = property.GetValue(y);
+				if (!xValue.Equals(yValue))
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+		public int GetHashCode(T obj)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
