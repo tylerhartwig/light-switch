5c5fc1574498de5e8f23143246a257b2524163b2
Tyler Hartwig <Tyler_hartwig@baylor.edu>
2017-03-30 07:56:16 -0500

    Add QuoteViewModel

---
+++ LightSwitch.UnitTests/ViewModels/QuoteViewModelTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Xunit;
+
+namespace LightSwitch.UnitTests
+{
+	public class QuoteViewModelTests
+	{
+		[Fact]
+		public void TestDefaultConstructor()
+		{
+			var quoteViewModel = new QuoteViewModel();
+			Assert.NotNull(quoteViewModel);
+		}
+
+		[Fact]
+		public void TestQuoteConstructor()
+		{
+			var quote = new Quote
+			{
+				ID = 777,
+				Text = "test test test text",
+				Reference = "Some guy said this"
+			};
+
+			var quoteViewModel = new QuoteViewModel(quote);
+			Assert.Equal(quote.Text, quoteViewModel.Text);
+			Assert.Equal(quote.Reference, quoteViewModel.Reference);
+		}
+
+		[Fact]
+		public void TestTextProperty()
+		{
+			var quoteViewModel = new QuoteViewModel();
+			var result = false;
+			quoteViewModel.PropertyChanged += (sender, e) =>
+			{
+				if (e.PropertyName == "Text")
+				{
+					result = true;
+				}
+			};
+
+			quoteViewModel.Text = "some test text";
+			Assert.True(result, "PropertyChanged was not raised for \"Text\"");
+		}
+
+		[Fact]
+		public void TestReferenceProperty()
+		{
+			var quoteViewModel = new QuoteViewModel();
+			var result = false;
+			quoteViewModel.PropertyChanged += (sender, e) =>
+			{
+				result = true;
+			};
+
+			quoteViewModel.Reference = "Some reference";
+			Assert.True(result, "PropertyChanged was not raised for \"Reference\"");
+		}
+	}
+}
