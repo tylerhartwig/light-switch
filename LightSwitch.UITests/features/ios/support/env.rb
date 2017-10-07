631221a695adb0c9a3dcadaa98bc44a8a4416d4e
Tyler Hartwig <tyler_hartwig@baylor.edu>
2017-10-06 19:50:48 -0500

    Setup for remote UI testing

--- LightSwitch.UITests/features/ios/support/env.rb
+++ LightSwitch.UITests/features/ios/support/env.rb
@@ -1,6 +1,6 @@
 require 'rspec/expectations'
 require 'appium_lib'
-require 'cucumber/ast'
+require 'cucumber'
 require File.join(File.dirname(__FILE__), '..', '..', '..', 'lib', 'light_switch')
 
 # Custom world object
@@ -9,11 +9,14 @@ end
 
 device_identifier = ENV['DEVICE']
 
-if device_identifier=='iOS Simulator'
+if device_identifier == 'iOS_Simulator'
   caps = Appium.load_appium_txt file: File.expand_path("./../../device_config/#{device_identifier}.txt",__FILE__), verbose: true
 else
   caps = Appium.load_appium_txt file: File.expand_path("./../../../default_device_config.txt",__FILE__), verbose: true
 end
+
+url = 'http://192.168.1.67:4723/wd/hub'
+# all_options = caps.merge(appium_lib: { server_url: url })
 
 Appium::Driver.new(caps)
 Appium.promote_appium_methods AppiumWorld
